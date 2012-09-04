2a31386585b67396afd4ee755da43a97b41806b8
yupengli <yupengli@uga.edu>
2012-09-04 17:43:13 -0400

    V0.1.1 small bug fixed

--- net/run.split_cluster.sh
+++ net/run.split_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 perl split_cluster.pl ../data/soybean_CytoscapeInput-nodes-all.txt ../data/medicago_CytoscapeInput-nodes-all.txt ../data/lotus_CytoscapeInput-nodes-all.txt ../data/soybean.tab ../data/medicago.tab ../data/lotus.tab ../data/all.evals
-mail -s "[lab] clusters were splited" liyupeng111@gmail.com
+wc -l log | mail -s "[lab] clusters were splited" liyupeng111@gmail.com
