aa1a982b7837c227afb6f97eb08f728abb1a1522
Bioops <bioops.info@gmail.com>
2015-09-02 11:56:04 -0400

    Update run.split_cluster.sh

--- net/run.split_cluster.sh
+++ net/run.split_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 perl split_cluster.pl ../data/soybean_CytoscapeInput-nodes-all.txt ../data/medicago_CytoscapeInput-nodes-all.txt ../data/lotus_CytoscapeInput-nodes-all.txt ../data/soybean.tab ../data/medicago.tab ../data/lotus.tab ../data/3legume.evals 1>log 2>err
-wc -l log | mail -s "[lab] clusters were splited" liyupeng111@gmail.com
+wc -l log | mail -s "[lab] clusters were splited" ...@gmail.com
